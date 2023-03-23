7eed8560b7ae126025d5a1876cd86345213e271b
Fernando Hoyos <122779323+ferhoyos@users.noreply.github.com>
2023-03-23 10:26:17 +0100

    Refine eslint config and format files (#4)
    * Refine eslint config and format files
    
    * Upgrade @types/react to version 17

--- src/components/Overview/ControlPlaneVersionbadge.tsx
+++ src/components/Overview/ControlPlaneVersionbadge.tsx
@@ -4,13 +4,13 @@ import * as React from 'react';
 export type ControlPlaneVersionBadgeProps = {
   version: string;
   isCanary?: boolean;
-  style?: React.CSSProperties
+  style?: React.CSSProperties;
 };
 
 export const ControlPlaneVersionBadge = (props: ControlPlaneVersionBadgeProps) => {
-    return (
-        <Label style={props.style} color={props.isCanary ? 'blue' : 'orange'} isCompact>
-          {props.version}
-        </Label>
-    );
+  return (
+    <Label style={props.style} color={props.isCanary ? 'blue' : 'orange'} isCompact>
+      {props.version}
+    </Label>
+  );
 };
