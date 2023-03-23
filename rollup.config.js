8c8e3ebce4007e6f2b47506751b6ecb90cbc9b0e
Alberto Gutierrez <aljesusg@gmail.com>
2023-03-23 15:50:11 +0100

    install plugin-json for axios calls

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import { readFileSync } from "fs";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
 
@@ -25,6 +26,7 @@ export default {
     peerDepsExternal(),
     resolve(),
     commonjs(),
+    json(),
     typescript({ useTsconfigDeclarationDir: true }),
     postcss({
       extensions: [".css"],
